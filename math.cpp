31f6ccb2337f299f1de29af11c8c41684e6f5634
Yiyun Qin <yiyun.qin@mths.ca>
2022-03-03 19:25:29 +0000

    math program

---
+++ math.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2022 Yiyun Qin All rights reserved
+//
+// Created by Yiyun Qin
+// Created at March 2022
+// This is a math program with proper style
+
+#include <iostream>
+
+int main() {
+    // This function calculates the circumference of a circle
+    const float TAU = 6.28;
+    int radius;
+    float circumference;
+
+    // input
+    std::cout << "Enter the length of the radius: ";
+    std::cin >> radius;
+
+    // process
+    circumference = TAU * radius;
+
+    // output
+    std::cout << std::endl;
+    std::cout << "Circumference is " << circumference << " mm²." << std::endl;
+
+    std::cout << "\nDone." << std::endl;
+}
